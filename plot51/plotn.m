3e61986ffb66c06a4d997ec80d5fff541ae9f6f3
abhimanyupallavisudhir <43954672+abhimanyupallavisudhir@users.noreply.github.com>
2018-12-21 14:48:51 +0530

    smaller interval

---
+++ plot51/plotn.m
@@ -0,0 +1,21 @@
+clear, clc;
+
+hold on
+for n = 0:25:100
+    mat = zeros(n + 1, n + 1);
+    vec = zeros(n + 1, 0);
+
+    vec(2, 1) = 1;
+    for i = 0:n
+        for j = 0:n
+            mat(i + 1, j + 1) = (1 - (-1)^(i + j + 1))/(i + j + 1);
+        end
+    end
+
+    a = mat\vec;
+    pola = fliplr(a');
+
+    x = -1:0.001:1;
+    plot(x, polyval(pola, x));
+end
+hold off
